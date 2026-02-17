53121c966860450d5ab1cfb8634c2bbd51c37d14
UserGit-Dev <welcometorapture@hotmail.it>
2026-02-17 15:43:01 +0100

    2d

---
+++ 2026-02-17/prova1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Inserisci il primo numero: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Inserisci il secondo numero: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+{
+    Console.WriteLine($"{a} è più grande di {b}");
+} else
+{
+    Console.WriteLine($"{b} è più grande di {a}");
+}
