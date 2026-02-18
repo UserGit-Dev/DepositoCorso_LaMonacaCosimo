07d0a5cf2d672ee97a71dbe2f1eb93a889b238c9
UserGit-Dev <welcometorapture@hotmail.it>
2026-02-18 18:01:45 +0100

    3g
    Smetto di usare "Console.WriteLine()" multipli per le spaziature. 
    Era un adattamento necessario finché eravamo a zero con gli altri, 
    ma ora che la sintassi è chiara a tutti (escape sequence), si passa a un codice più pulito.

---
+++ 2026-02-18/prove2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Creiamo un array di 5 elementi per contenere i voti
+        int[] voti = new int[5];
+
+        // Inserimento dei voti da tastiera
+        for (int i = 0; i < voti.Length; i++)
+        {
+            Console.Write($"Inserisci il voto {i + 1}: ");
+            voti[i] = int.Parse(Console.ReadLine()!);
+        }
+
+        // Calcolo della media
+        int somma = 0;
+        for (int i = 0; i < voti.Length; i++)
+        {
+            somma += voti[i];
+        }
+
+        float media = (float)somma / voti.Length;
+        Console.WriteLine($"La media dei voti è: {media}");
+    }
+}
---
+++ 2026-02-18/prove3/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] num = [1, 2, 3, 4, 5];
+
+foreach (int i in num)
+{
+    Console.WriteLine(i);
+}
+
+
+string a = "prova";
+Console.Write(a.Replace("va", "na"));
